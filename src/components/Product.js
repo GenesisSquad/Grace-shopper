02086685da5e466f8cd1b12b269cb3f5e92d54e0
Oggup <edifoo@gmail.com>
2021-04-05 12:17:21 -0700

    forgot to include the update for Product js file earlier that gets rendered within the shopping cart so I have included the latest code for it

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,24 +1,110 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { blue } from "@material-ui/core/colors";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import RemoveShoppingCartIcon from "@material-ui/icons/RemoveShoppingCart";
 
-const Product = ({ drinkItems, token }) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: blue[500],
+  },
+}));
+
+const Product = (drinkItems, token) => {
   const { productId } = useParams();
   const product = drinkItems.find((product) => productId === product.id);
-  <h1>HELLO can you see me</h1>
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   if (product) {
     return (
-      <>
-        <h2>Beverage</h2>
-        <div key={product.id} style={{ border: "1px solid black" }}>
-          <h5>{product.title}</h5>
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Rhino Avatar" className={classes.avatar}>
+              RC
+            </Avatar>
+          }
+          title={product.name}
+          subheader={product.price}
+        />
+        <CardMedia
+          className={classes.media}
+          image="https://www.incimages.com/uploaded_files/image/1920x1080/getty_500740897_200013331653767347294_333325.jpg"
+          title="Beverage"
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Product Category: {product.category}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="addShopping cart icon">
+            <AddShoppingCartIcon />
+          </IconButton>
+          <IconButton aria-label="addShopping cart icon">
+            <RemoveShoppingCartIcon />
+          </IconButton>
 
-          <div>Description : {product.description}</div>
-          <div>Price: {product.price}</div>
-        </div>
-      </>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            {" "}
+            More info:
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph></Typography>
+            <Typography paragraph></Typography>
+            <Typography paragraph></Typography>
+            <Typography paragraph>{product.description}</Typography>
+            <Typography></Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
     );
   } else {
     return <h1>LOADING :0)</h1>;
   }
 };
-export default Product ;
+
+export default Product;
