33a22cd08887ade6145e304c7d5d247de6b2fc59
Oggup <edifoo@gmail.com>
2021-04-08 14:52:02 -0700

    a little update with the file location cant get Product to render on Header so I commented it out

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 //import and re-export all components here
 import { default as ColorButton } from "./ColorButton";
 import { default as Header } from "./Header"; 
-import { default as Product } from "./Product"; 
-import { default as Products } from "./Products"; 
 
-export { ColorButton, Header, Product, Products };
+
+export { ColorButton, Header,};
