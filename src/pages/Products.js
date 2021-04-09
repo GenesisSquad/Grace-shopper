c21a1b7c6d81f5507777f3fe3aa981f62921e328
Oggup <edifoo@gmail.com>
2021-04-09 14:54:54 -0700

    Fixed a bug that wouldn't resize cards correctly

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,59 +1,56 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import clsx from "clsx";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import { blue } from "@material-ui/core/colors";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import RemoveShoppingCartIcon from "@material-ui/icons/RemoveShoppingCart";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import Popover from "@material-ui/core/Popover";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     marginTop: "20px",
+    
   },
   media: {
     height: 0,
     paddingTop: "56.25%",
   },
-  expand: {
-    transform: "rotate(0deg)",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: "rotate(360deg)",
-  },
-  avatar: {
-    backgroundColor: blue[500],
-  },
-  paper: {},
+  card:{},
+  paper: {height:"40px"},
   bottomCard: { justifyContent: "space-between" },
 }));
 
 const Products = ({ products, userData }) => {
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
   const handleAddItem = () => {
     console.log("added item!!!");
   };
   const handleRemoveItem = () => {
     console.log("item removed!!!");
   };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   return (
     <div className={classes.root}>
       <Grid
@@ -65,54 +62,61 @@ const Products = ({ products, userData }) => {
       >
         {products.map((product) => {
           return (
-            <Grid item xs={2}>
-              <Paper className={classes.paper}>
-                <Card className={classes.root}>
-                  <CardHeader title={product.name} subheader={product.price} />
-                  <CardMedia
-                    className={classes.media}
-                    image={product.imageURL}
-                    title="Beverage"
-                  />
-                  <CardContent>
-                    <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      {product.category}
-                    </Typography>
-                  </CardContent>
+            <Grid item xs={5} wrap="wrap"sm={3}>
+              {/* <Paper className={classes.paper}> */}
+              <Card className={classes.card}>
+                <CardHeader title={product.name} subheader={product.price} />
+                <CardMedia
+                  className={classes.media}
+                  image={product.imageURL}
+                  title="Beverage"
+                />
+                <CardContent>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {product.category}
+                  </Typography>
+                </CardContent>
+                <CardActions className={classes.bottomCard} disableSpacing>
                   <div>
-                    <CardActions className={classes.bottomCard} disableSpacing>
-                      <div>
-                        <IconButton aria-label="addShopping cart icon">
-                          <AddShoppingCartIcon onClick={handleAddItem} />
-                        </IconButton>
-                        <IconButton aria-label="remove shopping cart icon">
-                          <RemoveShoppingCartIcon onClick={handleRemoveItem} />
-                        </IconButton>
-                      </div>
-                      <IconButton
-                        className={clsx(classes.expand, {
-                          [classes.expandOpen]: expanded,
-                        })}
-                        onClick={handleExpandClick}
-                        aria-expanded={expanded}
-                        aria-label="show more"
-                      >
-                        {" "}
-                        <ExpandMoreIcon />
-                      </IconButton>
-                    </CardActions>
+                    <IconButton aria-label="addShopping cart icon">
+                      <AddShoppingCartIcon onClick={handleAddItem} />
+                    </IconButton>
+                    <IconButton aria-label="remove shopping cart icon">
+                      <RemoveShoppingCartIcon onClick={handleRemoveItem} />
+                    </IconButton>
                   </div>
-                  <Collapse in={expanded} timeout="auto" unmountOnExit>
+                  <Button
+                    aria-describedby={id}
+                    variant="contained"
+                    color="primary"
+                    onClick={handleClick}
+                  >
+                    More info
+                  </Button>
+                  <Popover
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "center",
+                    }}
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "center",
+                    }}
+                  >
                     <CardContent>
                       <Typography paragraph>{product.description}</Typography>
                     </CardContent>
-                  </Collapse>
-                </Card>
-              </Paper>
+                  </Popover>
+                </CardActions>
+              </Card>
+              {/* </Paper> */}
             </Grid>
           );
         })}
