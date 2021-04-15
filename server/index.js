aa05f6fd963c5bfe0a7a08b1e29f753a31d670e3
jeahya22 <jeahya22@gmail.com>
2021-04-15 12:00:33 -0700

    Stripe integration

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const jwt = require('jsonwebtoken');
 const { usersRouter } = require("./users");
 const { productsRouter } = require("./products");
 const { ordersRouter } = require("./orders");
+const { stripeRouter } = require("./stripe");
 const {client, getUserById, rebuildDB} = require('../db');
 
 // all required locally made files go ^
@@ -69,6 +70,7 @@ server.use(async (req, res, next) => {
 server.use("/api/products", productsRouter);
 server.use("/api/users", usersRouter);
 server.use("/api/orders", ordersRouter);
+server.use("/api/stripe", stripeRouter);
 
 
 server.use('*', (req, res, next) => {
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,61 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router";
 
+//! Stripe start
+import { Route } from "react-router-dom";
+import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
+const STRIPE_KEY =
+'pk_test_51IgESEAwKF3ow8u8iWs1EZ7w7SOHNw8zGEZZJ7cErTdZJfyvQ5iBSzWlQNC4Ngrkb24u8AbPrNP8ezMm1WpY5hhe0086gjXKtA';
+const PAYMENT_URL = 'http://localhost:3001/api/stripe/pay';
+const CURRENCY = 'USD';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+      marginTop: theme.spacing(3),
+      marginBottom: theme.spacing(3),
+      padding: theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+          marginTop: theme.spacing(6),
+          marginBottom: theme.spacing(6),
+          padding: theme.spacing(3),
+      },
+  },
+  buttons: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+  },
+  button: {
+      marginTop: theme.spacing(3),
+      marginLeft: theme.spacing(1),
+  },
+}));
+
+const onToken = (amount) => async (token) => {
+  console.log('Token is:', token);
+  try {
+      const response = await axios.post(PAYMENT_URL, {
+          source: token.id,
+          currency: CURRENCY,
+          amount,
+      });
+      console.log('Success!', response);
+  } catch (error) {
+      console.error(error);
+  }
+};
+//! Stripe end
+
 const Cart = ({ cart, setCart }) => {
+  //! STRIPE styling start
+  const classes = useStyles();
+  //! STRIPE styling end
+
   const getTotalSum = () => {
     return cart.reduce(
       (sum, { cost, quantity }) => sum + parseFloat(cost.slice(1)) * quantity,
@@ -40,9 +94,23 @@ const Cart = ({ cart, setCart }) => {
             <button onClick={() => removeFromCart(product)}>Remove</button>
           </div>
         ))}
-      </div>
-
+      </div>     
       <div>Total Cost: ${getTotalSum()}</div>
+       {/* STRIPE start */}
+       <Route exact path="/pay">
+      <Paper className={classes.paper}>
+         <StripeCheckout
+          token={onToken(10000)}
+          stripeKey={STRIPE_KEY}
+          name="Rhino Coffee"
+          amount={10000 / 100}
+          currency={CURRENCY}
+          shippingAddress
+         />
+         <Button>Pay Order</Button>
+         </Paper>
+      </Route>
+      {/* STRIPE end */}
     </>
   );
 };
