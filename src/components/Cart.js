1b3a487a293bc68a823fb372d031836f8a7339fd
junoeye <47189364+junoeye@users.noreply.github.com>
2021-04-16 11:59:00 -0700

    Finished up Cart and stylings, cleaned up the Cart file as well as header file.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,86 +4,79 @@ import { callApi } from "../api";
 
 //! Stripe start
 import { Route } from "react-router-dom";
-import axios from 'axios';
-import StripeCheckout from 'react-stripe-checkout';
+import axios from "axios";
+import StripeCheckout from "react-stripe-checkout";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
 import { GridLoadIcon } from "@material-ui/data-grid";
-import { CircularProgress } from "@material-ui/core";
+import {
+  CircularProgress,
+  Grid,
+  Card,
+  TextField,
+  CardContent,
+  Divider,
+  Typography,
+} from "@material-ui/core";
 
 const STRIPE_KEY =
-'pk_test_51IgESEAwKF3ow8u8iWs1EZ7w7SOHNw8zGEZZJ7cErTdZJfyvQ5iBSzWlQNC4Ngrkb24u8AbPrNP8ezMm1WpY5hhe0086gjXKtA';
-const PAYMENT_URL = 'http://localhost:3001/api/stripe/pay';
-const CURRENCY = 'USD';
+  "pk_test_51IgESEAwKF3ow8u8iWs1EZ7w7SOHNw8zGEZZJ7cErTdZJfyvQ5iBSzWlQNC4Ngrkb24u8AbPrNP8ezMm1WpY5hhe0086gjXKtA";
+const PAYMENT_URL = "http://localhost:3001/api/stripe/pay";
+const CURRENCY = "USD";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  //! //////OG BELOW///
   paper: {
-      marginTop: theme.spacing(3),
-      marginBottom: theme.spacing(3),
-      padding: theme.spacing(2),
-      [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-          marginTop: theme.spacing(6),
-          marginBottom: theme.spacing(6),
-          padding: theme.spacing(3),
-      },
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(0),
+      marginBottom: theme.spacing(0),
+      padding: theme.spacing(1),
+      justifyContent: "center",
+    },
   },
   buttons: {
-      display: 'flex',
-      justifyContent: 'flex-end',
+    display: "flex",
+    justifyContent: "flex-end",
   },
   button: {
-      marginTop: theme.spacing(3),
-      marginLeft: theme.spacing(1),
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
   },
 }));
 
 const onToken = (amount) => async (token) => {
-  console.log('Token is:', token);
+  console.log("Token is:", token);
   try {
-      const response = await axios.post(PAYMENT_URL, {
-          source: token.id,
-          currency: CURRENCY,
-          amount,
-      });
-      console.log('Success!', response);
+    const response = await axios.post(PAYMENT_URL, {
+      source: token.id,
+      currency: CURRENCY,
+      amount,
+    });
+    console.log("Success!", response);
   } catch (error) {
-      console.error(error);
+    console.error(error);
   }
 };
 //! Stripe end
-const data = {
-  id: 3,
-  status: "created",
-  userId: 3,
-  datePlaced: "2021-04-15T18:29:18.482Z",
-  products: [
-    {
-      id: 9,
-      name: "joe",
-      description: "GREAT COFFEE!",
-      price: "$22",
-      imageURL:
-        "https://images.squarespace-cdn.com/content/v1/57b7c57b44024338a6700bdf/1588704248137-5U0TCBQRZCKTVVLM8QUO/ke17ZwdGBToddI8pDm48kA_SSaoz4elkj-HsZd8gX3Z7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UWPwZyNcweDIvdeL5kotwkIXjs9g0WibSO_cU-Ijy4Pwg6poS-6WGGnXqDacZer4yQ/IMG_3607.jpg",
-      inStock: true,
-      category: "TEA",
-      quantity: 5,
-    },
-    {
-      id: 5,
-      name: "Bella London Fog",
-      description:
-        "Delicious blend of Earl Grey tea with sultry oat milk foam. Notes of lavender and vanilla.",
-      price: "$1200",
-      imageURL:
-        "https://www.splenda.com/wp-content/themes/bistrotheme/assets/recipe-images/london-fog-tea-latte.jpg",
-      inStock: true,
-      category: "TEA",
-      quantity: 6,
-    },
-  ],
-};
 
 const Cart = ({ token, cart, setCart }) => {
   const [name, setName] = useState("");
@@ -104,7 +97,7 @@ const Cart = ({ token, cart, setCart }) => {
 
   const clearCart = () => {
     setCart([]);
-    localStorage.setItem('cart','')
+    localStorage.setItem("cart", "");
   };
 
   const setQuantity = (product, amount) => {
@@ -121,42 +114,76 @@ const Cart = ({ token, cart, setCart }) => {
     <>
       {cart && cart.length ? (
         <>
-          <h1>My Cart</h1>
-          <button onClick={clearCart}>Clear Cart</button>
-          <div className="products">
-            {cart.map((product, idx) => (
-              <div className="product" key={idx}>
-                <h3>{product.name}</h3>
-                <h4>${product.price}</h4>
-                <input
-                  value={product.quantity}
-                  onChange={(e) =>
-                    setQuantity(product, parseInt(e.target.value))
-                  }
-                />
-                <img src={product.image} alt={product.name} />
-                <button onClick={() => removeFromCart(product)}>Remove</button>
-              </div>
-            ))}
-          </div>
-
-          <div>Total price: ${getTotalSum()}</div>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="small"
+            onClick={clearCart}
+          >
+            Clear Cart
+          </Button>
+          <Grid>
+            <Card className={classes.root} variant="outlined">
+              <CardContent>
+                <div className="products">
+                  {cart.map((product, idx) => (
+                    <div className="product" key={idx}>
+                      {/* <h3>{product.name}</h3> */}
+                      <Typography variant="h5" component="h2">
+                        {product.name}
+                      </Typography>
+                      {/* <h4>{product.description}</h4> */}
+                      {/* <h4>{product.price}</h4> */}
+                      <Typography className={classes.pos} color="textSecondary">
+                        {product.price}
+                      </Typography>
+                      <input
+                        type="number"
+                        size="1"
+                        maxLength="1"
+                        value={product.quantity}
+                        onChange={(e) =>
+                          setQuantity(product, parseInt(e.target.value))
+                        }
+                      />
+                      {/* <img src={product.imageUrl} alt={product.name} /> */}
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        size="small"
+                        onClick={() => removeFromCart(product)}
+                      >
+                        Remove
+                      </Button>
+                      <Divider />
+                    </div>
+                  ))}
+                </div>
+                <br />
+                <br />
+                <Typography variant="h5" component="h2">
+                  Total: ${getTotalSum()}
+                </Typography>
+                {/* <div>Total price: ${getTotalSum()}</div> */}
+              </CardContent>
+            </Card>
+          </Grid>
           {/* STRIPE start */}
-      <Paper className={classes.paper}>
-         <StripeCheckout
-          token={onToken(10000)}
-          stripeKey={STRIPE_KEY}
-          name="Rhino Coffee"
-          amount={10000 / 100}
-          currency={CURRENCY}
-          shippingAddress
-         />
-         <Button>Pay Order</Button>
-         </Paper>
-      {/* STRIPE end */}
+          {/* <Paper className={classes.paper}> */}
+          <StripeCheckout
+            token={onToken(10000)}
+            stripeKey={STRIPE_KEY}
+            name="Rhino Coffee"
+            amount={10000 / 100}
+            currency={CURRENCY}
+            shippingAddress
+          />
+          {/* </Paper> */}
+          {/* STRIPE end */}
         </>
       ) : (
-        <CircularProgress />
+        <h3> Your cart is empty.</h3>
+        // <CircularProgress />
       )}
     </>
   );
